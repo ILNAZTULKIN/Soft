7a8a5a856790db15d1a6d7636a2521f97eebace0
norrex1337 <oshchepkovk@mail.ru>
2022-03-02 19:48:33 +0500

    Удаление столбцов

---
+++ deletecolumn.php
@@ -0,0 +1,20 @@
+/**
+ * This is an example header component. This component is meant to be placed at the top of the page.
+ *
+ * @name {String} name Удаление столбцов
+
+ * @author {String} author Ощепков Кирилл
+ *
+ */
+
+<?php
+	include "BaseVar.php";
+	
+	$tab = htmlentities(file_get_contents("tanos.txt"));
+	
+	$column_title=$_POST["spisok"];	
+	$sql="alter table $tab drop $column_title ";
+	$bd->query($sql);
+	$zapros = "SELECT * from $tab";
+	vivod($bd, $zapros, $tab);
+?>
