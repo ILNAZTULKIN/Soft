836301a44aec58755ffccf16e9c6e1e48e238b0c
norrex1337 <oshchepkovk@mail.ru>
2022-03-02 18:20:17 +0500

    Revert "Merge branch 'main' of https://github.com/ILNAZTULKIN/Soft"
    This reverts commit f0f52f21e390a9970642a18239c2552915efb1b5, reversing
    changes made to b593d3335491039154257ce16668fcf9f330f51e.

--- Errors_prog/501.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="css/Style.css"> 
-<?php 
-	echo "<form><center>501:не реализовано. <br/> Проверьте соединение c БД. <br/> <a href=\"/index.php\">Повторить</a></center></form>";
-?>
